a053e3fe7b2f611f2a7d62087d86012b7d7ce021
NaviLlicious <ianscottysullivan@hotmail.com>
2017-03-05 06:25:43 -0600

    Reworked Character Controller
    Nearly completed character controller with working jump and animations.

--- Caribbean/Assets/Main Game/Scripts/MMOCharController.cs
+++ Caribbean/Assets/Main Game/Scripts/MMOCharController.cs
@@ -5,49 +5,51 @@ using System.Collections.Generic;
 public class MMOCharController : MonoBehaviour 
 {
 
-	public Transform playerCam, character, centerPoint;
+	//public Transform playerCam, Player, centerPoint;
 
-	private float mouseX, mouseY;
+	//private float mouseX, mouseY;
 	public float mouseSensitivity = 10f;
 	public float mouseYPosition = 4f;
 
-	CharacterController player;
+	//private float zoom;
+	//public float zoomSpeed = 2;
 
-	private float moveFB, moveLR;
-	public float moveSpeed = 2f;
-
-	private float zoom;
-	public float zoomSpeed = 2;
-
-	public float zoomMin = -1.518f;
-	public float zoomMax = -8.94f;
+	//public float zoomMin = -1.518f;
+	//public float zoomMax = -8.94f;
 
 	public float rotationSpeed = 5f;
 
 	float verticalVelocity;
 
-	public float jumpDist = 5f;
+	public GameObject player;
+	public float xOffset = 0;
+	public float yOffset = 0;
+	public float zOffset = 0;
 
-	bool canJump;
+	private float mouseX, mouseY;
 
-	//ShipScriptVariable Begin
-
-	public bool CanMove;
-
-	//ShipScriptVariable End
+	private float zoom;
+	public float zoomSpeed;
+	public float zoomMin;
+	public float zoomMax;
 
 	// Use this for initialization
 	void Start () 
 	{
-		player = GetComponent<CharacterController> ();
 		zoom = -21.1f;
-		CanMove = true;
+		player = GameObject.FindGameObjectWithTag ("Player");
 	}
 
 
 	// Update is called once per frame
 	void Update () 
 	{
+
+		if (player != null) {
+			transform.position = new Vector3 (player.transform.position.x, player.transform.position.y, -10);
+		} else {
+			Debug.Log ("Player not set correctly.");
+		}
 
 
 			zoom += Input.GetAxis ("Mouse ScrollWheel") * zoomSpeed;
@@ -58,7 +60,7 @@ public class MMOCharController : MonoBehaviour
 			if (zoom < zoomMax)
 				zoom = zoomMax;
 
-			playerCam.transform.localPosition = new Vector3 (0, 0, zoom);
+			//playerCam.transform.localPosition = new Vector3 (0, 0, zoom);
 
 			if (Input.GetMouseButton (1)) {
 				mouseX += Input.GetAxis ("Mouse X");
@@ -66,60 +68,13 @@ public class MMOCharController : MonoBehaviour
 			}
 
 			mouseY = Mathf.Clamp (mouseY, -60f, 60f);
-			playerCam.LookAt (centerPoint);
-			centerPoint.localRotation = Quaternion.Euler (mouseY, mouseX, 0);
-
-			moveFB = Input.GetAxis ("Vertical") * moveSpeed;
-			moveLR = Input.GetAxis ("Horizontal") * moveSpeed;
-
-		if(CanMove){
-
-				Vector3 movement = new Vector3 (moveLR, verticalVelocity, moveFB);
-				movement = character.rotation * movement;
-				character.GetComponent<CharacterController> ().Move (movement * Time.deltaTime);
-				centerPoint.position = new Vector3 (character.position.x, character.position.y + mouseYPosition, character.position.z);
-
-				if (Input.GetAxis ("Vertical") > 0 | Input.GetAxis ("Vertical") < 0) {
-
-					Quaternion turnAngle = Quaternion.Euler (0, centerPoint.eulerAngles.y, 0);
-
-					character.rotation = Quaternion.Slerp (character.rotation, turnAngle, Time.deltaTime * rotationSpeed);
-
-				}
-			
-				if (Input.GetKeyDown ("space")) {
-					Debug.Log ("Jumped");
-					verticalVelocity += jumpDist;
-					canJump = false;
-				}
-
-				/*if (player.isGrounded == true) 
-			{
-				if (Input.GetKeyDown (KeyCode.Space)) 
-				{
-					verticalVelocity += jumpDist;
-					canJump = false;
-					Debug.Log ("Jumped");
-				}
-			}*/
-		}
+			//playerCam.LookAt (centerPoint);
+			//player.localRotation = Quaternion.Euler (mouseY, mouseX, 0);
 
 	}
 
 	void FixedUpdate()
 	{
-		if (CanMove) {
-			if (canJump = false) {
-				if (player.isGrounded == true) {
-					canJump = true;
-				}
-			}
-
-			if (player.isGrounded == false) {
-				verticalVelocity += Physics.gravity.y * Time.deltaTime;
-			} else {
-				verticalVelocity = 0f;
-			}
-		}
+		
 	}
 }
---
+++ Caribbean/Assets/Main Game/Scripts/Movement.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+
+public class Movement : MonoBehaviour {
+
+	Animator anim;
+
+	bool isWalking = false;
+	bool isJumping = true;
+
+	const float walkSpeed = .25f;
+
+	void Awake() 
+	{
+		anim = GetComponent<Animator> ();
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Walking ();	
+		Turning ();
+		Running();
+		Jump ();
+	}
+
+	void Turning()
+	{
+		anim.SetFloat ("Turning", Input.GetAxis ("Horizontal"));
+	}
+
+	void Walking()
+	{
+		if (Input.GetKeyDown (KeyCode.Backslash)) 
+		{
+			//Toggle Walk
+			isWalking = !isWalking;
+			anim.SetBool ("Walk", isWalking);
+		}
+	}
+
+	void Running()
+	{
+		if (anim.GetBool ("Walk"))
+		{
+			anim.SetFloat ("MoveX", Mathf.Clamp (Input.GetAxis ("MoveX"), -walkSpeed, walkSpeed));
+			anim.SetFloat ("MoveZ", Mathf.Clamp (Input.GetAxis ("MoveZ"), -walkSpeed, walkSpeed));
+		} 
+		else 
+		{
+			anim.SetFloat ("MoveZ", Input.GetAxis ("MoveZ"));
+			anim.SetFloat ("MoveX", Input.GetAxis ("MoveX"));
+		}
+	}
+
+	void Jump()
+	{
+		/*if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			anim.SetTrigger ("Jump");
+		}*/
+
+		if (Input.GetButtonDown ("Jump") && !isJumping) 
+		{
+			anim.SetTrigger ("Jump");
+		}
+
+		if (anim.GetCurrentAnimatorStateInfo (0).IsName ("JumpAnim")) {
+			isJumping = true;
+		} else {
+			isJumping = false;
+		}
+	}
+}
